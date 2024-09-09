fd5d2261a4e7ee30d89ad2c87729053f6f8772d0
RVVTeles <rafael.teles2@gmail.com>
2024-09-09 16:59:56 -0300

    Adição de novo arquivo de DB

--- TestDB/contasapagar.sql
+++ TestDB/contasapagar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2024 at 09:48 PM
+-- Generation Time: Sep 09, 2024 at 09:57 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `clientes` (
   `cpf` varchar(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
-  `numero_telefone` int(11) NOT NULL COMMENT 'Número de telefone do cliente',
+  `numero_telefone` varchar(20) NOT NULL COMMENT 'Número de telefone do cliente',
   `endereco` varchar(255) NOT NULL,
-  `numero_endereco` varchar(10) NOT NULL,
+  `numero_endereco` varchar(20) NOT NULL,
   `cidade` varchar(255) NOT NULL,
   `estado` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,13 +42,10 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`cpf`, `nome`, `numero_telefone`, `endereco`, `numero_endereco`, `cidade`, `estado`) VALUES
-('1', 'Abner', 2147483647, 'Rua Capitão Marechal Tenente', '5012', 'Fortaleza', 'Ceará'),
-('12345678999', 'Joaquim', 2147483647, 'Rua tal', '82', 'Fortaleza', 'Ceará'),
-('143225568-7', 'Adabervaldo Torres', 0, '', '', '', ''),
-('2', 'b', 0, '', '', '', ''),
-('3', 'c', 0, '', '', '', ''),
-('4', 'd', 0, '', '', '', ''),
-('5', 'g', 0, '', '', '', '');
+('12345678900', 'Ana Paula Silva', '85987654321', 'Rua das Flores', '123', 'Fortaleza', 'Ceará'),
+('23456789011', 'Carlos Eduardo Santos', '85976543210', 'Avenida Beira Mar', '456', 'Fortaleza', 'Ceará'),
+('34567890122', 'Maria Oliveira Costa', '85965432109', 'Rua do Sol', '789', 'Fortaleza', 'Ceará'),
+('45678901233', 'João Pedro Almeida', '85954321098', 'Rua das Acácias', '101', 'Fortaleza', 'Ceará');
 
 -- --------------------------------------------------------
 
@@ -71,14 +68,10 @@ CREATE TABLE `contas` (
 --
 
 INSERT INTO `contas` (`id`, `cliente_cpf`, `valor`, `juros`, `data_emissao`, `data_vencimento`, `data_pagamento`) VALUES
-(1, '3', 200, 1, '2024-08-23', '2024-09-13', '2024-09-09'),
-(2, '1', 5000, 1, '2024-08-23', '2024-09-13', '2024-09-26'),
-(4, '1', 100, 1, '2024-08-23', '2024-09-13', NULL),
-(5, '1', 100, 1, '2024-08-23', '2024-09-13', NULL),
-(6, '1', 100, 1, '2024-08-23', '2024-08-23', '2024-09-05'),
-(7, '1', 100, 1, '2024-08-23', '2024-09-13', NULL),
-(8, '2', 56, 1, '2024-08-26', '2024-08-27', NULL),
-(10, '143225568-7', 500, 10, '2024-09-05', '2024-09-19', NULL);
+(1, '12345678900', 5000, 10, '2024-09-09', '2024-09-10', NULL),
+(2, '12345678900', 1500, 10, '2024-09-02', '2024-09-11', NULL),
+(3, '12345678900', 2000, 30, '2024-09-02', '2024-09-03', NULL),
+(4, '12345678900', 750, 10, '2024-09-02', '2024-09-04', '2024-09-06');
 
 --
 -- Indexes for dumped tables
@@ -105,7 +98,7 @@ ALTER TABLE `contas`
 -- AUTO_INCREMENT for table `contas`
 --
 ALTER TABLE `contas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
